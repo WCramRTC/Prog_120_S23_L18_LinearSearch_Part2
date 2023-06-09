2fa4232409697681eb9cfc2c661cad7aff45ac99
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-06-08 18:10:33 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prog_120_S23_L18_LinearSearch_Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Prog_120_S23_L18_LinearSearch_Part2
+ 
